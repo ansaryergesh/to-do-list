2c0cd5be69bd55e436ff735d4bdc8d057fa2d93b
ansaryergesh <yergeshovansar@gmail.com>
2020-01-27 21:07:12 +0600

    addaptive blocks

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,19 +1,26 @@
 const display = (() => {
     
     const sidebar = document.createElement('aside');
-    sidebar.classList.add('left', 'col','s12');
+    sidebar.classList.add('left', 'col', 'm3','s12');
     const head = document.createElement('h2');
+    const head2 = document.createElement('h2');
     head.classList.add('head')
+    head2.classList.add('head2')
     const addBtn = document.createElement('button');
     addBtn.classList.add('addBtn');
-    const body = document.querySelector('body');
-    body.appendChild(sidebar);
+    const tasks = document.createElement('section');
+    tasks.classList.add('tasks', 'col','m8', 's12');
+    const row = document.querySelector('.row');
+    row.appendChild(sidebar);
     sidebar.appendChild(head);
     sidebar.appendChild(addBtn);
+    row.appendChild(tasks);
+    tasks.appendChild(head2);
 
     const side = () => {
         head.innerText =' List of projects: ';
         addBtn.innerText = 'Add new project';
+        head2.innerText = 'Tasks:';
 
     };
 
