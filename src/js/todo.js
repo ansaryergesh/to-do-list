197f0d1cc4936145b54bc7217a046dfb0869d4a7
ansaryergesh <yergeshovansar@gmail.com>
2020-02-05 04:15:39 +0600

    fixing linters

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,4 +1,4 @@
 export const todolist = ( title, description, dueDate, priority ) => {
-  return ({ id: Math.floor(Math.random() * 1000).toString(), title: title, description: description,
-    dueDate: dueDate, priority: priority, done: false });
+  return { id: Math.floor(Math.random() * 1000).toString(), title: title, description: description,
+    dueDate: dueDate, priority: priority, done: false };
 };
