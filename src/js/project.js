49a8e58851147931c3aecd874e81198ee2a13eb8
ansaryergesh <yergeshovansar@gmail.com>
2020-02-05 07:07:53 +0600

    fixing linters

--- src/js/project.js
+++ src/js/project.js
@@ -1,8 +1,8 @@
 export const project = (name) => {
   const pr = {
     id: Math.floor(Math.random() * 1000).toString(),
-    name: name,
-    tasks: []
+    name,
+    tasks: [],
   };
   return pr;
 };
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,11 +1,11 @@
 export const todolist = (title, description, dueDate, priority) => {
   const todo = {
     id: Math.floor(Math.random() * 1000).toString(),
-    title: title,
-    description: description,
-    dueDate: dueDate,
-    priority: priority,
-    done: false 
+    title,
+    description,
+    dueDate,
+    priority,
+    done: false,
   };
   return todo;
 };
